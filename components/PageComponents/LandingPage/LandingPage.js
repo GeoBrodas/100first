d761f5bfbda9076f8c4c258d0f5bcc8ce8824328
GeoBrodas <geobro2310@gmail.com>
2021-12-30 10:36:30 +0530

    style landing

--- components/PageComponents/LandingPage/LandingPage.js
+++ components/PageComponents/LandingPage/LandingPage.js
@@ -5,24 +5,34 @@ function LandingPage() {
   return (
     <NavLanding>
       {/* Hero */}
-      <div className="flex flex-col items-center">
-        <div className="items-center flex">
-          <h2 className="hero-landing">A platform for the official</h2>
-          <span>
-            <Image
-              src="/Logo/100daysofcode.png"
-              width="300px"
-              height="200px"
-              alt="landing"
-            />
-          </span>
-          <h2 className="hero-landing">challenge!</h2>
+      <div className="flex items-center space-x-2">
+        <div className="flex text-right flex-col px-5 space-y-4 my-20">
+          <div className="flex">
+            <h2 className="hero-landing">
+              A platform for the official
+              <span className="text-gradient font-extrabold text-3xl mx-2">
+                #100daysofcode
+              </span>
+              challenge!
+            </h2>
+          </div>
+
+          {/* headline */}
+          <p className="text-2xl text-white italic underline decoration-[#c9e265]">
+            Complete and keep track of each day of the challenge.
+          </p>
         </div>
 
-        {/* headline */}
-        <p className="text-2xl text-white italic underline decoration-[#c9e265]">
-          Complete and keep track of each day
-        </p>
+        {/* Image */}
+        <div className="h-1/3 w-1/2">
+          <Image
+            alt="landing-image"
+            width={100}
+            height={60}
+            src="/Logo/100daysofcode.png"
+            layout="responsive"
+          />
+        </div>
       </div>
     </NavLanding>
   );
