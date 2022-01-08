858939ab5e18e44c8211ad1ce0f5b58c516caac2
GeoBrodas <geobro2310@gmail.com>
2022-01-08 09:24:54 +0530

    fix bug finally!! -problem in serverside changing redirects

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -74,7 +74,7 @@ export async function getServerSideProps(context) {
   if (!session) {
     return {
       redirect: {
-        destination: '/auth/sign-in',
+        destination: '/',
         permanent: false,
       },
     };
