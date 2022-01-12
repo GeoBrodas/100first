95f5334d96aa601d6ed3f9291cebd2df0adc0627
GeoBrodas <geobro2310@gmail.com>
2022-01-12 22:20:15 +0530

    final tests with next-pwa -3

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
-const withPwa = require('next-pwa');
+// const withPwa = require('next-pwa');
 
-module.exports = withPwa({
+module.exports = {
   reactStrictMode: true,
-  pwa: {
-    dest: 'public',
-    register: false,
-    // skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
+  // pwa: {
+  //   dest: 'public',
+  //   register: false,
+  //   // skipWaiting: true,
+  //   disable: process.env.NODE_ENV === 'development',
+  // },
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
-});
+};
