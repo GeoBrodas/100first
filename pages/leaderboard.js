ff5192c040e3e9d5d7c2ede5b1456cd91e6bb06d
GeoBrodas <geobro2310@gmail.com>
2022-01-12 23:45:24 +0530

    add coming soon page to /leaderboard

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -1,5 +1,15 @@
+import NavLanding from '@/Layouts/NavLanding';
+import { MdConstruction } from 'react-icons/md';
+
 function LeaderBoardPage() {
-  return <div></div>;
+  return (
+    <NavLanding>
+      <div className="grid h-screen place-content-center">
+        <MdConstruction className="text-white mx-auto w-20 h-20" />
+        <h1 className="text-center text-white text-2xl">Coming soon....</h1>
+      </div>
+    </NavLanding>
+  );
 }
 
 export default LeaderBoardPage;
