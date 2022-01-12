c398c809a787fb183a97f8231f39148f3d339f9d
GeoBrodas <geobro2310@gmail.com>
2022-01-12 13:55:17 +0530

    configure pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    runtimeCaching: true,
+    buildExcludes: [/middleware-manifest.json$/],
+    disable: process.env.NODE_ENV === 'development',
+  },
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
