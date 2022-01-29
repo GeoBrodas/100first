15c44affac4db49952255241dd763870cca8088c
GeoBrodas <geobro2310@gmail.com>
2022-01-29 08:13:43 +0530

    test pwa config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,28 @@
-// const withPwa = require('next-pwa');
+const withPwa = require('next-pwa');
 
-module.exports = {
+module.exports = withPwa({
   reactStrictMode: true,
-  // pwa: {
-  //   dest: 'public',
-  //   register: false,
-  //   // skipWaiting: true,
-  //   disable: process.env.NODE_ENV === 'development',
-  // },
+  pwa: {
+    dest: 'public',
+    register: false,
+    skipWaiting: true,
+    runtimeCaching: [
+      {
+        urlPattern: /\.(?:json|xml|csv)$/i,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'static-data-assets',
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+    ],
+
+    disable: process.env.NODE_ENV === 'development',
+  },
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
-};
+});
